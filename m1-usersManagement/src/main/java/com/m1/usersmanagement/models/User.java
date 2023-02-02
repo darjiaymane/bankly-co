6453c41f1018f32236b46df7ced1e5bfc3bef820
Darji Aymane <aymanedarji7@gmail.com>
2023-02-02 14:24:34 +0100

    Create the user entity

--- m1-usersManagement/src/main/java/com/m1/usersmanagement/models/User.java
+++ m1-usersManagement/src/main/java/com/m1/usersmanagement/models/User.java
@@ -1,2 +1,87 @@
-package com.m1.usersmanagement.models;public class User {
+package com.m1.usersmanagement.models;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @UniqueElements
+    private String email;
+    @NotEmpty
+    private String password;
+    @Column(unique = true)
+    private String CIN;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, String CIN) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.CIN = CIN;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCIN() {
+        return CIN;
+    }
+
+    public void setCIN(String CIN) {
+        this.CIN = CIN;
+    }
 }
