5763e9ecbe901fd63665a8e3d08b8ffab1b260c0
Darji Aymane <aymanedarji7@gmail.com>
2023-02-02 14:37:55 +0100

    Create ResponseDTO

--- m1-usersManagement/src/main/java/com/m1/usersmanagement/DTO/ResponseDTO.java
+++ m1-usersManagement/src/main/java/com/m1/usersmanagement/DTO/ResponseDTO.java
@@ -1,2 +1,42 @@
-package com.m1.usersmanagement.DTO;public class ResponseDTO {
+package com.m1.usersmanagement.DTO;
+
+public class ResponseDTO {
+    private String status;
+    private String message;
+    private Object data;
+
+    public ResponseDTO() {
+    }
+
+    public ResponseDTO(String message, Object data, String status) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+    public ResponseDTO(String message, String status) {
+        this.status = status;
+        this.message = message;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
